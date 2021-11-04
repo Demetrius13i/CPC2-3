d65e62574ef4ac4248b4a1cf801c046808b6fad9
Demetrius13i <S.d-dimon@mail.ru>
2021-11-04 17:23:17 +0300

    First try create action

--- app/src/main/java/com/dmitryi/example/cpc2_3/MainActivity.kt
+++ app/src/main/java/com/dmitryi/example/cpc2_3/MainActivity.kt
@@ -2,10 +2,28 @@ package com.dmitryi.example.cpc2_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var backgroundstart = (ImageView).findViewById(R.id.backgroundstart)
+        var startButton = (ImageButton).findViewById(R.id.startButton)
     }
+
+    public fun onStartButtonClick(View view: View) {
+        startButton.setVisibility(View.GONE)
+        backgroundstart.setVisibility(View.GONE)
+        startButton.setClickable(false)
+    }
+
+
+
 }
